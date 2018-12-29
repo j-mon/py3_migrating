153d889e1dc8fd978656411edfd82c8670a37b19
Jeff Monsalve <monsalve.jeffrey@gmail.com>
2018-12-29 01:21:07 -0500

    Undescores PEP-515

---
+++ pep-515_underscore.py
@@ -0,0 +1,12 @@
+#grouping decimal numbers by thousands 
+one_million = 1_000_000
+
+#grouping hexadecimal addresses by words 
+addr = 0xCAFE_F00D
+
+#grouping bits into nibbles in a binary literal
+flags = 0b_001_1111_0100_1110
+
+# same, for string conversions
+flags = int('0b_1111_0000', 2)
+

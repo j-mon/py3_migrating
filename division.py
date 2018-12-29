8310df0d194c318cd946825d94ea567ff837888a
Jeff Monsalve <monsalve.jeffrey@gmail.com>
2018-12-29 01:29:46 -0500

    divsion

---
+++ division.py
@@ -0,0 +1,5 @@
+data = pandas.read_csv('timing.csv')
+velocity = data['distance'] / data['time'] #references features
+
+#floor division
+n_gifts = money // gift_price #correct for int and float arguments
